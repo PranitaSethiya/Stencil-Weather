314170902247a17178eb5afcbb7b24054543d8d7
Pranita Setiya <pranitasethiya@gmail.com>
2015-08-06 15:13:33 +0530

    added new class for splash

---
+++ app/src/main/java/com/saphion/stencilweather/activities/SplashActivity.java
@@ -0,0 +1,18 @@
+package com.saphion.stencilweather.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by sachin on 5/8/15.
+ */
+public class SplashActivity extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+
+    }
+}
